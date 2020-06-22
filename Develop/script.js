8734fb58e1bee5d4664ae789dc9edaab6da443ef
durganp1 <durganp1@gmail.com>
2020-06-22 14:08:37 -0500

    add password size prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,24 @@
-// Assignment code here
+// create a series of prompts for password criteria
+//  *length 8-128 characters - (window.prompt)
+//  *character types - checkboxes for each choice - one prompt
+//    * lowercase
+//    * uppercase
+//    * numeric
+//    * special charecters
+// validate that at least one character type was selected
+// generate random password based off selected types
+// display password in alert or in window
 
-
+var passwordSize = function(event) {
+  var promptSize = prompt("How many characters would you like your password to be?  Please choose between 8 - 128");
+    if (promptSize < 8 || promptSize > 128 || !promptSize) {
+      alert("You need to make a valid selection.");
+      return passwordSize();
+    }
+    else if (promptSize > 8 && promptSize < 128) {
+      alert("You have chosen " + promptSize + " characters.")
+    }
+  };
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -14,4 +32,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passwordSize);
