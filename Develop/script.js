91f34c0261f436b799a73bd96f7d58f6335fa9ca
durganp1 <durganp1@gmail.com>
2020-06-22 17:50:04 -0500

    add conditions for password selection

--- Develop/script.js
+++ Develop/script.js
@@ -9,9 +9,15 @@
 // generate random password based off selected types
 // display password in alert or in window
 
+var upperCaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numbersArr = ["1","2","3","4","5","6","7","8","9","0"];
+var symbolsArr = ["!","@","#","$","%","^","&","*"];
+
 var passwordSize = function(event) {
   var promptSize = prompt("How many characters would you like your password to be?  Please choose between 8 - 128");
-    if (promptSize < 8 || promptSize > 128 || !promptSize) {
+  console.log(promptSize);  
+  if (promptSize < 8 || promptSize > 128 || !promptSize) {
       alert("You need to make a valid selection.");
       return passwordSize();
     }
@@ -19,7 +25,7 @@ var passwordSize = function(event) {
       alert("You have chosen " + promptSize + " characters.")
     }
     characterSelection();
-  };
+};
 var characterSelection = function() {
   var confirmUpperCase = confirm("Would you like to use UPPERCASE in your password?");
   var confirmLowerCase = confirm("Would you like to use lowercase in your password?");
@@ -27,20 +33,64 @@ var characterSelection = function() {
   var confirmSymbols = confirm("Would you like to use symbols in your password?");
     if (confirmUpperCase === false && confirmLowerCase === false && confirmNumbers === false && confirmSymbols === false) {
       alert("You need to choose at least one option!");
-      return false;
+      characterSelection();
     }
-}
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+    else {
 
-// Write password to the #password input
+    }
+    
+};
+
+     
+    
+
+var userChoices = function() {
+  var userPassword = ""
+  
+  if (characterSelection.confirmUpperCase && userPassword < promptSize) {
+    userPassword = userPassword + upperCaseArr[Math.floor(Math.random() * upperCaseArr.length)]
+    userPassword += upperCaseArr
+  }
+  else if (userPassword === promptSize){
+    alert("Your new password is " + userPassword);
+  };
+  if (characterSelection.confirmLowerCase && userPassword < promptSize) {
+    userPassword = userPassword + lowerCaseArr[Math.floor(Math.random() * lowerCaseArr.length)]
+    userPassword += lowerCaseArr
+  }
+  else if (userPassword === promptSize){
+    alert("Your new password is " + userPassword);
+  };
+  if (characterSelection.confirmNumbers && userPassword < promptSize) {
+    userPassword = userPassword + numbersArr[Math.floor(Math.random() * numbersArr.length)]
+    userPassword += numbersArr
+  }
+  else if (userPassword === promptSize){
+    alert("Your new password is " + userPassword);
+  };
+  if (characterSelection.confirmSymbols && userPassword < promptSize) {
+    userPassword = userPassword + symbolsArr[Math.floor(Math.random() * symbolsArr.length)]
+    userPassword += symbolsArr
+  }
+  else if (userPassword === promptSize){
+    alert("Your new password is " + userPassword);
+  };
+  console.log(userPassword).value;
+  return;
+
+};
+  
+// Get references to the #generate element
+ var generateBtn = document.querySelector("#generate");
+
+// //Write password to the #password input
 // function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+//   var password = userChoices;
+//   var passwordText = document.querySelector("#userChoices");
 
 //   passwordText.value = password;
 
-// }
+// };
 
 // Add event listener to generate button
 //generateBtn.addEventListener("click", writePassword);
