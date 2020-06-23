aa2be6f71cc7d2e7dcb67e079faf920e75029409
durganp1 <durganp1@gmail.com>
2020-06-23 09:00:45 -0500

    bugs in password.  only using uppercase

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 // create a series of prompts for password criteria
-//  *length 8-128 characters - (window.prompt)
-//  *character types - checkboxes for each choice - one prompt
-//    * lowercase
-//    * uppercase
-//    * numeric
-//    * special charecters
+//  *length 8-128 characters - (window.prompt)
+//  *character types - checkboxes for each choice - one prompt
+//    * lowercase
+//    * uppercase
+//    * numeric
+//    * special charecters
 // validate that at least one character type was selected
 // generate random password based off selected types
 // display password in alert or in window
@@ -13,85 +13,87 @@ var upperCaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 var lowerCaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbersArr = ["1","2","3","4","5","6","7","8","9","0"];
 var symbolsArr = ["!","@","#","$","%","^","&","*"];
-
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmNumbers;
+var confirmSymbols;
+var userPassword = "";
 var passwordSize = function(event) {
-  var promptSize = prompt("How many characters would you like your password to be?  Please choose between 8 - 128");
-  console.log(promptSize);  
-  if (promptSize < 8 || promptSize > 128 || !promptSize) {
-      alert("You need to make a valid selection.");
-      return passwordSize();
-    }
-    else if (promptSize > 8 && promptSize < 128) {
-      alert("You have chosen " + promptSize + " characters.")
-    }
-    characterSelection();
-};
-var characterSelection = function() {
-  var confirmUpperCase = confirm("Would you like to use UPPERCASE in your password?");
-  var confirmLowerCase = confirm("Would you like to use lowercase in your password?");
-  var confirmNumbers = confirm("Would you like to use numbers in your password?");
-  var confirmSymbols = confirm("Would you like to use symbols in your password?");
+  var promptSize = prompt("How many characters would you like your password to be?  Please choose between 8 - 128"); 
+    if (promptSize < 8 || promptSize > 128 || !promptSize) {
+      alert("You need to make a valid selection.");
+      return passwordSize();
+    }
+    else if (promptSize > 8 && promptSize < 128) {
+      alert("You have chosen " + promptSize + " characters.")
+    };
+    characterSelection(promptSize);
+}; 
+var characterSelection = function(promptSize) {  
+    confirmUpperCase = confirm("Would you like to use UPPERCASE in your password?");
+    confirmLowerCase = confirm("Would you like to use lowercase in your password?");
+    confirmNumbers = confirm("Would you like to use numbers in your password?");
+    confirmSymbols = confirm("Would you like to use symbols in your password?");
     if (confirmUpperCase === false && confirmLowerCase === false && confirmNumbers === false && confirmSymbols === false) {
-      alert("You need to choose at least one option!");
-      characterSelection();
-    }
-    else {
+      alert("You need to choose at least one option!");
+      characterSelection();
+    }
+    else {
+      userChoices(promptSize);
+    }
+};
+var userChoices = function(promptSize) {
+    debugger;
+    if (confirmUpperCase === true) {
+   for (var i = userPassword.length; i < promptSize; i++) {
+      var upperPassword = upperCaseArr[Math.floor(Math.random() * upperCaseArr.length)];
+      userPassword += upperPassword;
+    };
+  };
+    // else {
+    //   alert("Your new password is " + userPassword);
+    //   return;
+    // }
 
+  
+    if (confirmLowerCase === true) {
+  for (var i = userPassword.length; i < promptSize; i++) {
+      var lowerPassword = lowerCaseArr[Math.floor(Math.random() * lowerCaseArr.length)];
+      userPassword += lowerPassword;
+    };
+  };
+    // else {
+    //   alert("Your new password is " + userPassword);
+    //   return;
+    // }
+  
+  
+    if (confirmNumbers === true) {
+  for (var i = userPassword.length; i < promptSize; i++) {
+      var numberPassword = numbersArr[Math.floor(Math.random() * numbersArr.length)];
+      userPassword += numberPassword;
+    };
+  };
+    // else {
+    //   alert("Your new password is " + userPassword);
+    //   return;
+    // }
+
+  
+    if (confirmSymbols === true) {
+  for (var i = userPassword.length; i < promptSize; i++) {
+      var symbolPassword = symbolsArr[Math.floor(Math.random() * symbolsArr.length)];
+      userPassword += symbolPassword;
+    };
+  }
+    else if 
+      (userPassword.length === promptSize); {
+      alert("Your new password is " + userPassword);
+      return;
     }
-    
+  
 };
 
-     
-    
-
-var userChoices = function() {
-  var userPassword = ""
-  
-  if (characterSelection.confirmUpperCase && userPassword < promptSize) {
-    userPassword = userPassword + upperCaseArr[Math.floor(Math.random() * upperCaseArr.length)]
-    userPassword += upperCaseArr
-  }
-  else if (userPassword === promptSize){
-    alert("Your new password is " + userPassword);
-  };
-  if (characterSelection.confirmLowerCase && userPassword < promptSize) {
-    userPassword = userPassword + lowerCaseArr[Math.floor(Math.random() * lowerCaseArr.length)]
-    userPassword += lowerCaseArr
-  }
-  else if (userPassword === promptSize){
-    alert("Your new password is " + userPassword);
-  };
-  if (characterSelection.confirmNumbers && userPassword < promptSize) {
-    userPassword = userPassword + numbersArr[Math.floor(Math.random() * numbersArr.length)]
-    userPassword += numbersArr
-  }
-  else if (userPassword === promptSize){
-    alert("Your new password is " + userPassword);
-  };
-  if (characterSelection.confirmSymbols && userPassword < promptSize) {
-    userPassword = userPassword + symbolsArr[Math.floor(Math.random() * symbolsArr.length)]
-    userPassword += symbolsArr
-  }
-  else if (userPassword === promptSize){
-    alert("Your new password is " + userPassword);
-  };
-  console.log(userPassword).value;
-  return;
-
-};
-  
 // Get references to the #generate element
- var generateBtn = document.querySelector("#generate");
-
-// //Write password to the #password input
-// function writePassword() {
-//   var password = userChoices;
-//   var passwordText = document.querySelector("#userChoices");
-
-//   passwordText.value = password;
-
-// };
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", passwordSize);
+var generateBtn = document.querySelector("#generate");
+    generateBtn.addEventListener("click", passwordSize);
